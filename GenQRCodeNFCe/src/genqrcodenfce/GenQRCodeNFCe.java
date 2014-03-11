2d7eed0d69aac37a21b7f420796492583e7ca7ed
Renato Krause <renatokrause@gmail.com>
2014-03-11 11:15:36 -0300

    2014-03-11 Updates manhã

--- GenQRCodeNFCe/src/genqrcodenfce/GenQRCodeNFCe.java
+++ GenQRCodeNFCe/src/genqrcodenfce/GenQRCodeNFCe.java
@@ -19,6 +19,9 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 package genqrcodenfce;
 
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+
 /**
  *
  * @author Renato Krause <renatokrause@gmail.com>
@@ -30,7 +33,18 @@ public class GenQRCodeNFCe {
      */
     public static void main(String[] args) throws Exception {
         QRCodeNFCe qRCodeNFCe = new QRCodeNFCe();
-
+        qRCodeNFCe.setChNFe("43120910585504000174650010000000541123456781");
+        qRCodeNFCe.setNVersao("100");
+        qRCodeNFCe.setTpAmb("2");
+        qRCodeNFCe.setDhEmi("2012-09-27T16:20:34-03:00");
+        qRCodeNFCe.setVNF("1000.00");
+        qRCodeNFCe.setVICMS("180.00");
+        qRCodeNFCe.setDigVal("37327151612b623074616f514f3966414a7766646c5875715176383d");
+        qRCodeNFCe.setCIdToken("000001");
+        qRCodeNFCe.setToken("1058550420130001");
+        qRCodeNFCe.calcCHashQRCode();
+        BufferedImage bufferedImage = qRCodeNFCe.generateQRCode("https://www.sefaz.rs.gov.br/NFCE/NFCE-COM.aspx?", 0, 0);
+        ImageIO.write(bufferedImage, "jpg", new java.io.File("C:\\Users\\renatoj\\Documents\\GitHub\\EmissorNFCe\\GenQRCodeNFCe\\output.jpg"));
     }
     
 }
